8cc51101be885e05394e184349077a882697e87b
Kohli0003 <aakarshitkohli26@gmail.com>
2024-05-03 20:28:47 +0530

    Added hide and show for points other than index

--- hand.py
+++ hand.py
@@ -16,6 +16,7 @@ click_interval = 2  # Set the minimum time interval between clicks (in seconds)
 
 # Initialize variables for smoothing
 smoothed_cursor_x, smoothed_cursor_y = 0, 0
+flag = False  # Initialize flag as False
 
 while True:
     _, image = cam.read()
@@ -25,6 +26,10 @@ while True:
 
     if results.multi_hand_landmarks:
         for hand_landmark in results.multi_hand_landmarks:
+            k = cv2.waitKey(1)  # Move this inside the loop to capture key presses continuously
+            if k == ord('a'):  # Check if 'a' key is pressed
+                flag=not flag
+
             index_4 = hand_landmark.landmark[4]
             index_8 = hand_landmark.landmark[8]
             index_12 = hand_landmark.landmark[12]
@@ -43,26 +48,27 @@ while True:
             smoothed_cursor_y = (smoothed_cursor_y + cursor_y) / 2
 
             pyautogui.moveTo(int(smoothed_cursor_x), int(smoothed_cursor_y))
-
-            cv2.circle(image, (x4, y4), 5, (255, 0, 0), -1)  # Draw circle for index 4
             cv2.circle(image, (x8, y8), 5, (0, 0, 255), -1)  # Draw circle for index 8
-            cv2.circle(image, (x12, y12), 5, (0, 255, 255), -1)
-            cv2.circle(image, (x16, y16), 5, (255, 255, 255), -1)
 
-            distance = math.sqrt((x8 - x4) ** 2 + (y8 - y4) ** 2)
-            distance2 = math.sqrt((x12 - x4) ** 2 + (y12 - y4) ** 2)
-            distance3 = math.sqrt((x16 - x4) ** 2 + (y16 - y4) ** 2)
+            if flag:  # Check if flag is True
+                cv2.circle(image, (x4, y4), 5, (255, 0, 0), -1)  # Draw circle for index 4
+                cv2.circle(image, (x12, y12), 5, (0, 255, 255), -1)
+                cv2.circle(image, (x16, y16), 5, (255, 255, 255), -1)
 
-            if distance < 22 and (time.time() - last_click_time) > click_interval:
-                print("click")
-                pyautogui.click()
-                last_click_time = time.time()
-            elif distance2 < 22:
-                print("double click")
-                pyautogui.doubleClick()
-            elif distance3 < 30:
-                print("tab switch")
-                pyautogui.hotkey('command', 'tab')
+                distance = math.sqrt((x8 - x4) ** 2 + (y8 - y4) ** 2)
+                distance2 = math.sqrt((x12 - x4) ** 2 + (y12 - y4) ** 2)
+                distance3 = math.sqrt((x16 - x4) ** 2 + (y16 - y4) ** 2)
+
+                if distance < 22 and (time.time() - last_click_time) > click_interval:
+                  print("click")
+                  pyautogui.click()
+                  last_click_time = time.time()
+                elif distance2 < 22:
+                  print("double click")
+                  pyautogui.doubleClick()
+                elif distance3 < 30:
+                  print("tab switch")
+                  pyautogui.hotkey('command', 'tab')
 
     cv2.imshow("Hand Gesture Control", image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
